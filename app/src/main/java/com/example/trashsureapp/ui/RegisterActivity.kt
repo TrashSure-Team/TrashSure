c64a80b982c5f4ef240aa8a33be5bbfd733eec27
dimaradenis <dimaradenis02@gmail.com>
2023-12-10 19:36:52 +0700

    TAMBAH WELCOME , LOGIN , REGISTER

---
+++ app/src/main/java/com/example/trashsureapp/ui/RegisterActivity.kt
@@ -0,0 +1,23 @@
+package com.example.trashsureapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trashsureapp.databinding.ActivityRegisterBinding
+
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txtLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+    }
+}
